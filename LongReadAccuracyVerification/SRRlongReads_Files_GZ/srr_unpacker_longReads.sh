4716aff87be0eb6110374ecabdf930eeac698cca
Zachary <60785837+SykesZachary@users.noreply.github.com>
2020-04-03 18:37:25 +0100

    Delete srr_unpacker_longReads.sh

--- LongReadAccuracyVerification/SRRlongReads_Files_GZ/srr_unpacker_longReads.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for SRR in $(cat SRR_longReads_AccList.txt); do
-  # -X 20000 added to limit the amount of reads dumped for testing
-  # Organism: H.sapien
-  fastq-dump -X 20000 --split-files --gzip -I -O \
-  "/home/sykes/LongReadAccuracyVerification/SRRlongReads_Files_GZ/" $SRR
-done
-
